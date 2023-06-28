9dcb4f98c48d6cd7815a378b12a6efedee722f82
Renaud De Pessemier <renaud@depessemier.tech>
2023-06-27 23:50:40 -0400

    Docker configuration files

---
+++ Docker/dockerdns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# We had modify the Ubuntu BIND image to customize it according to our requirements.
+
+docker container prune --force
+for i in {50..80}
+do 
+docker run -itd  --network=TP-BRIDGE --ip=192.168.2.$i depat/bind
+done
+
+
